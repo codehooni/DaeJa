e40cbcf30cd12690e6ad5fbff42aacc927722b13
codehooni <jihooni0113@gmail.com>
2023-11-14 21:45:32 +0900

    Nineth.1 Commit (Jihoon) -> error

--- app/src/main/java/com/app/daeja/Activity/RecomendActivity.java
+++ app/src/main/java/com/app/daeja/Activity/RecomendActivity.java
@@ -1,19 +1,33 @@
 package com.app.daeja.Activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.daeja.Activity.Domain.ParkingInfo;
 import com.app.daeja.Adapter.ParkingInfoAdapter;
 import com.app.daeja.R;
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapGpsManager;
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
 
 import java.util.ArrayList;
@@ -23,6 +37,8 @@ import retrofit2.Call;
 
 public class RecomendActivity extends AppCompatActivity {
 
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private TMapGpsManager tMapGpsManager;
     Thread thread;
     Call<List<ParkingInfo>> call;
 
@@ -31,8 +47,8 @@ public class RecomendActivity extends AppCompatActivity {
     private LinearLayout linearLayoutTmap;
     private List<ParkingInfo> parkingInfos;
     private TMapView tMapView;
-    private double cur_lat = 37.4849732;
-    private double cur_lng = 126.9012370;
+    private double cur_lat = 37.5663507;
+    private double cur_lng = 126.9851113;
     private static final String tApiKey = "KbtV6K1LiCa2kYZ2ieDhU3pxBBS5A5gA5CL5O3el";
 
     @Override
@@ -40,7 +56,15 @@ public class RecomendActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recomend);
 
-        tMapViewInit();
+        //tMapViewInit();
+
+        // 위치 권한 확인 및 요청
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            // 권한이 이미 부여되었으면 지도 설정 시작
+            tMapViewInit();
+        }
 
         callServer();
         try {
@@ -52,6 +76,7 @@ public class RecomendActivity extends AppCompatActivity {
         pointPin();
         tMapView.setLocationPoint(37.48395, 126.9010);
         recyclerVieParking();
+
     }
 
     private void pointPin() {
@@ -113,6 +138,55 @@ public class RecomendActivity extends AppCompatActivity {
 
     private void tMapViewInit() {
         linearLayoutTmap = findViewById(R.id.linearLayoutTmap);
+        drawPath();
+
+        // TMapGpsManager를 사용하여 현재 위치 설정
+        tMapGpsManager = new TMapGpsManager(this);
+        tMapGpsManager.setMinTime(1000);
+        tMapGpsManager.setMinDistance(5);
+        tMapGpsManager.setProvider(TMapGpsManager.GPS_PROVIDER);
+        tMapGpsManager.OpenGps();
+
+        // 현재 위치를 나타내는 마커 추가
+        TMapMarkerItem currentLocationMarker = new TMapMarkerItem();
+        currentLocationMarker.setIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_gray));
+        tMapView.addMarkerItem("currentLocationMarker", currentLocationMarker);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                // 현재 위치의 좌표를 가져와서 마커 위치 업데이트
+                Log.d("Location", "Latitude: " + location.getLatitude() + ", Longitude: " + location.getLongitude());
+
+                cur_lat = location.getLatitude();
+                cur_lng = location.getLongitude();
+                TMapPoint currentLocation = new TMapPoint(cur_lat, cur_lng);
+                tMapView.getMarkerItemFromID("currentLocationMarker").setTMapPoint(currentLocation);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                // 위치 공급자 상태 변경 시 호출
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                // 위치 공급자가 사용 가능한 상태로 변경 시 호출
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                // 위치 공급자가 사용 불가능한 상태로 변경 시 호출
+            }
+        };
+
+        // 위치 업데이트를 요청
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, locationListener);
+        }
 
         tMapView = new TMapView(this);
         tMapView.setSKTMapApiKey(tApiKey);
@@ -123,6 +197,21 @@ public class RecomendActivity extends AppCompatActivity {
         tMapView.setCenterPoint(cur_lng, cur_lat);
 
         linearLayoutTmap.addView(tMapView);
+    }
+
+    // 경로 찍는 메서드
+    private void drawPath() {
+        TMapPoint Start = new TMapPoint(37.21668666666667, 127.03513166666667); // 울집
+        TMapPoint End = new TMapPoint(37.5663507, 126.9851113); // 목적지   //37.5663507, 126.9851113 서울타워  //37.322235, 127.12765166666667 단국대
+
+        try {
+            TMapPolyLine tMapPolyLine = new TMapData().findPathData(Start, End);
+            tMapPolyLine.setLineColor(Color.BLUE);
+            tMapPolyLine.setLineWidth(2);
+            tMapView.addTMapPolyLine("Line1", tMapPolyLine);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void recyclerVieParking() {
